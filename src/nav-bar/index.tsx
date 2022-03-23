1e2d9eb49926f57a8ad2bdff3de6cfee96f0034d
sdttttt <sdttttt@163.com>
2022-03-23 17:25:55 +0800

    feat: Add grid

--- src/nav-bar/index.tsx
+++ src/nav-bar/index.tsx
@@ -1,9 +1,21 @@
+import { h } from "preact";
 import classes from "./index.module.scss";
+import { Icon, Button, Grid } from "semantic-ui-react";
 
 export default function NavBar() {
 	return (
 		<div class={classes["navbar-container"]}>
-			此处为顶栏
+			<Grid>
+				<Grid.Column floated="left" width={1}>
+					<Button icon>
+						<Icon name="world" />
+					</Button>
+
+					<Button icon>
+						<Icon name="world" />
+					</Button>
+				</Grid.Column>
+			</Grid>
 		</div>
 	);
 }
