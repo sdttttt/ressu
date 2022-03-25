f0d53ff60b886e9a17df9f8150ee9966acc2c719
sdttttt <sdttttt@163.com>
2022-03-25 10:59:42 +0800

    feat: Add rss url input.

--- src/app.tsx
+++ src/app.tsx
@@ -5,21 +5,22 @@ import classes from "./app.module.scss";
 import { Grid } from "@mui/material";
 
 export function App() {
+	
 	return (
 		<>
 			<div className={classes.navbar}>
 				<NavBar></NavBar>
 			</div>
-			<Grid container item className={classes.main} columns={24}>
-				<Grid md={6} xs={8} alignContent="center">
+			<Grid container className={classes.main} columns={24}>
+				<Grid item  md={6} xs={8} alignContent="center">
 					<FeedSidebar></FeedSidebar>
 				</Grid>
 
-				<Grid md={6} xs={8} alignContent="center">
+				<Grid item md={6} xs={8} alignContent="center">
 					<FeedItems></FeedItems>
 				</Grid>
 
-				<Grid md={24 - 6 - 6} xs={24 - 8 - 8} alignContent="center">
+				<Grid item md={24 - 6 - 6} alignContent="center">
 					这里是内容栏
 				</Grid>
 			</Grid>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
+import { join } from "path";
 import react from '@vitejs/plugin-react'
+
+function resolve(path: string) {
+	return join(__dirname, path);
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+	plugins: [react()],
+
+	resolve: {
+		alias: {
+			"@": resolve("src"),
+			"redux": resolve("redux"),
+		}
+	}
 })
