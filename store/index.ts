182d3b5a82652e1cca7489be4654a718b1df1a62
760159537 <760159537@qq.com>
2022-03-31 11:26:13 +0800

    feat(redux): Add thunk.

--- store/index.ts
+++ store/index.ts
@@ -1,4 +1,5 @@
 import reducers from "./reducers";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import thunkMiddleware from 'redux-thunk'
 
-export default createStore(reducers);
+export default createStore(reducers, applyMiddleware(thunkMiddleware));
