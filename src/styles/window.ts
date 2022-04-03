8158f661c0a676b374329c6cd9d0437b109cc116
Lin <sdttttt@icloud.com>
2022-04-03 09:32:41 +0800

    test(window): Add unit test.

--- src/styles/window.ts
+++ src/styles/window.ts
@@ -10,3 +10,25 @@ export function computeFixedLeftDistanceFromPercent(percent: number): string {
 	return `${(windoWidth - divWidth) / 2}px`;
 
 }
+
+// @ts-ignore
+if (import.meta.vitest) {
+	// @ts-ignore
+	const { it, expect, assert } = import.meta.vitest;
+	it("Math.sqrt()", () => {
+		expect(Math.sqrt(4)).toBe(2);
+		expect(Math.sqrt(144)).toBe(12);
+		expect(Math.sqrt(2)).toBe(Math.SQRT2);
+	});
+	it("JSON", () => {
+		const input = {
+			foo: "hello",
+			bar: "world"
+		};
+
+		const output = JSON.stringify(input);
+
+		expect(output).eq('{"foo":"hello","bar":"world"}');
+		assert.deepEqual(JSON.parse(output), input, "matches original");
+	});
+}
