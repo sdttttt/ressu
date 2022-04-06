05d56d4f9576909c6072328b953d2e0405ccbdbf
760159537 <760159537@qq.com>
2022-04-06 16:30:38 +0800

    feat(wasm): http_get return str.

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -34,10 +34,12 @@ pub fn hello() {
 }
 
 #[wasm_bindgen]
-pub async fn http_get(url: String) {
+pub async fn http_get(url: String) -> String {
 	let res = reqwest::get(url.as_str()).await.expect("http get Error");
 	console_log!("Status: {}", res.status());
 
 	let body = res.text().await.expect("response to utf-8 text");
 	console_log!("Body:\n\n{}", body);
+	
+	body
 }
