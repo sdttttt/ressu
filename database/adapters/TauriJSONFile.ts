0b548ae57e63bf0b181874fca5deeaa75a9bb46e
760159537 <760159537@qq.com>
2022-04-25 16:30:33 +0800

    feat(databse): add tauri fs adapter for lowdb.

---
+++ database/adapters/TauriJSONFile.ts
@@ -0,0 +1,23 @@
+import { Adapter } from 'lowdb/lib/Low'
+import { TauriTextFile } from './TauriTextFile'
+
+export class JSONFile<T> implements Adapter<T> {
+    #adapter: TauriTextFile
+
+    constructor(filename: string) {
+        this.#adapter = new TauriTextFile(filename)
+    }
+
+    async read(): Promise<T | null> {
+        const data = await this.#adapter.read()
+        if (data === null) {
+            return null
+        } else {
+            return JSON.parse(data) as T
+        }
+    }
+
+    write(obj: T): Promise<void> {
+        return this.#adapter.write(JSON.stringify(obj, null, 2))
+    }
+}
