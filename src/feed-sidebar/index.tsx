9caf76b9fc2fd320b2f5644027c7c210ff4273c0
760159537 <760159537@qq.com>
2022-04-27 15:32:54 +0800

    feat(sidebar): Add channels jsx.

--- src/feed-sidebar/index.tsx
+++ src/feed-sidebar/index.tsx
@@ -9,12 +9,13 @@ export default function FeedSidebar() {
 
 	const channels = useSelector(selectChannels)
 
+	const channelsJSX = channels.map(t => (<div> {t.title} </div>));
 
 	return (
 		<div className={classes["sidebar-container"]}>
 			<RessuInput prefix={<SearchIcon />}></RessuInput>
 
-			{ channels.length === 0 ? <h3> 暂无订阅源 </h3> : <></> }
+			{ channels.length === 0 ? <h3> 暂无订阅源 </h3> : channelsJSX}
 		</div>
 	);
 
