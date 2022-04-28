3c13f1fa6c3b80f3eebe91763a42a21b0aadd166
sdttttt <sdttttt@163.com>
2022-04-28 10:55:30 +0800

    test(database): Add unit test.

--- database/feeds.ts
+++ database/feeds.ts
@@ -3,13 +3,62 @@ import { JSONFile } from "./adapters/TauriJSONFile"
 import { FEED_DB_FILENAME } from "./names";
 import { clone } from "lodash-es"
 
-import type { Feeds } from "../store/typing"
+import { Feeds } from "../store/typing"
 
-const feedDB = new Low(new JSONFile<Feeds>(FEED_DB_FILENAME)); 
+const feedDB = new Low(new JSONFile<Feeds>(FEED_DB_FILENAME));
 
 /**
  * Feed persistence
  */
 export async function feedsDataLocalSync(data: Feeds) {
-    
+	const realLocalData = clone(data);
+	realLocalData.channels = data.channels.map(
+		channel => {
+			const result = clone(channel);
+			result.posts = [];
+			return result;
+		}
+	)
+
+	feedDB.data = realLocalData;
+	return await feedDB.write()
+}
+
+// @ts-ignore
+if (import.meta.vitest) {
+	const {
+		it,
+		expect
+		// @ts-ignore
+	} = import.meta.vitest;
+
+	it('feed copy but not copy posts.', () => {
+		const feeds: Feeds = {
+			channels: [
+				{
+					title: "1",
+					url: "1.1.1",
+					description: "111",
+					postSize: 1,
+					posts: [{ title: "hellow", url: "hellow", content: "", date: "1" }]
+				}
+			]
+		}
+
+		const realLocalData = clone(feeds);
+		realLocalData.channels = feeds.channels.map(
+			channel => {
+				const result = clone(channel);
+				result.posts = [];
+				return result;
+			}
+		)
+
+		expect(realLocalData.channels.length).toBe(1);
+		expect(realLocalData.channels[0].title).toBe("1");
+		expect(realLocalData.channels[0].url).toBe("1.1.1");
+		expect(realLocalData.channels[0].description).toBe("111");
+		expect(realLocalData.channels[0].postSize).toBe(1);
+		expect(realLocalData.channels[0].posts.length).toBe(0);
+	})
 }
--- vitest.config.ts
+++ vitest.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     // ...
-    includeSource: ['src/**/*.{js,ts}'],
+    includeSource: ['src/**/*.{js,ts}', 'store/**/*.{js,ts}', "database/*.{js,ts}"],
     environment: "happy-dom"
   },
 })
