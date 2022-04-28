bfa2bde1a5418858eb20d0e53287d9f9a961fe52
sdttttt <sdttttt@163.com>
2022-04-28 15:32:20 +0800

    feat(reducers): Add initialize action to feed.

--- store/feeds.ts
+++ store/feeds.ts
@@ -6,39 +6,44 @@ import { runWASM } from "@/utils/wasm";
 import { toaster } from "evergreen-ui";
 
 const initialState: Feeds = {
-    channels: [],
+	channels: [],
 };
 
 /**
  * async add new rss channel.
  */
 export const addRSSChannelAsync = createAsyncThunk('channels/add', async (url: string) => {
-    if (isURL(url)) {
-        const metaInfo = await runWASM(() => getFeedMetaFormUrl(url));
-        if (metaInfo.isSpecification()) {
-            console.log(metaInfo.json());
-            return metaInfo.json();
-        } else {
-            toaster.danger("该订阅源不是RSS2.0标准, 尚不支持其他标准的RSS.")
-        }
-    }
+	if (isURL(url)) {
+		const metaInfo = await runWASM(() => getFeedMetaFormUrl(url));
+		if (metaInfo.isSpecification()) {
+			console.log(metaInfo.json());
+			return metaInfo.json();
+		} else {
+			toaster.danger("该订阅源不是RSS2.0标准, 尚不支持其他标准的RSS.")
+		}
+	}
 })
 
 const feedsSlice = createSlice({
-    name: "channels",
-    initialState,
-    reducers: {
-        remove: (state, url: PayloadAction<string>) => {
-            const { channels } = state
-            state.channels = channels.filter(rss => rss.url != url.payload);
-        },
-    },
+	name: "channels",
+	initialState,
+	reducers: {
 
-    extraReducers: (builder) => {
-        builder.addCase(addRSSChannelAsync.fulfilled, (state, actions) => {
-            console.log(actions.payload, state);
-        })
-    }
+		initialize: (state, { payload }: PayloadAction<Feeds>) => {
+			state.channels = payload.channels;
+		},
+
+		remove: (state, url: PayloadAction<string>) => {
+			const { channels } = state
+			state.channels = channels.filter(rss => rss.url != url.payload);
+		},
+	},
+
+	extraReducers: (builder) => {
+		builder.addCase(addRSSChannelAsync.fulfilled, (state, actions) => {
+			console.log(actions.payload, state);
+		})
+	}
 });
 
 export const selectFeeds = (state: RessuStore) => state.feeds;
