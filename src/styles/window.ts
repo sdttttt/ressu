452e909d48108efb45234a8f49336fefb8084742
Lin <sdttttt@icloud.com>
2022-04-30 11:37:50 +0800

    test: remove test.

--- src/styles/window.ts
+++ src/styles/window.ts
@@ -9,11 +9,3 @@ export function computeFixedLeftDistanceFromPercent(percent: number): string {
 	const divWidth = windoWidth * (percent / 100);
 	return `${(windoWidth - divWidth) / 2}px`;
 }
-
-
-if (import.meta.vitest) {
-
-	const { it, expect, assert } = import.meta.vitest;
-
-
-}
