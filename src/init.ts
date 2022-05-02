d9990b42e62314755251477874b1088397000ece
sdttttt <sdttttt@icloud.com>
2022-05-02 23:50:55 +0800

    fix: build problom.

--- src/init.ts
+++ src/init.ts
@@ -2,6 +2,9 @@ import { createDir } from "@tauri-apps/api/fs";
 import { appDir, join } from "@tauri-apps/api/path";
 import { DATABASES_PATH } from "@database/names";
 
-const rootPathAsync = await appDir();
+(async () => {
+	const rootPathAsync = await appDir();
+	await createDir(await join(rootPathAsync, DATABASES_PATH), { recursive: true });
+})()
 
-await createDir(await join(rootPathAsync, DATABASES_PATH), { recursive: true });
+
