b43e6ec059645c1d3926ab4d87bef953f0548390
sdttttt <sdttttt@icloud.com>
2022-05-03 09:33:49 +0800

    feat: chunk optimize.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 import { join } from "path";
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
 import { visualizer } from "rollup-plugin-visualizer";
 import banner from "vite-plugin-banner";
 
@@ -9,26 +9,48 @@ function resolve(path: string) {
 }
 
 function isDev() {
-	const mode = process.env.NODE_ENV || 'development'
-	return mode === 'development';
+	const mode = process.env.NODE_ENV || "development";
+	return mode === "development";
 }
-
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), visualizer(), banner(`
+	plugins: [
+		react(),
+		visualizer(),
+		banner(`
 		@sdttttt: nothing.
-	`)],
+	`)
+	],
 	clearScreen: false,
 	build: {
 		minify: !isDev(),
 		sourcemap: isDev() ? "inline" : false,
+
+		rollupOptions: {
+			output: {
+				manualChunks(id) {
+					if (id.includes("node_modules")) {
+
+						if (id.includes("lodash-es")) {
+							return "lodash-es";
+						}
+
+						if (id.includes("evergreen-ui")) {
+							return "evergreen-ui";
+						}
+
+						return "vendor";
+					}
+				}
+			}
+		}
 	},
 	resolve: {
 		alias: {
 			"@": resolve("src"),
 			"@store": resolve("store"),
-			"@database": resolve("database"),
+			"@database": resolve("database")
 		}
 	}
-})
+});
